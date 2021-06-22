cc9072c1a1a6da122f9ab81f5a843408759a3132
rudeboy45 <45rudeboy@gmail.com>
2021-06-22 13:13:29 +0300

    up

---
+++ Concentration/ConcentrationGame 3.swift
@@ -0,0 +1,60 @@
+//
+//  ConcentrationGame.swift
+//  Concentration
+//
+//  Created by Evgen on 17.02.2021.
+//  Copyright © 2021 Nikko. All rights reserved.
+//
+
+import Foundation
+
+
+class ConcentrationGame {
+    private(set) var cards = [Card]()
+    
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchingIndex = indexOfOneAndOnlyFaceUpCard, matchingIndex != index {
+                if cards[matchingIndex].identifier == cards[index].identifier {
+                    cards[matchingIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            } else {
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "ConcentrationGam.init(\(numberOfPairsOfCards): must have at least one pairs of cards")
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card,card]
+            
+        }
+        // TODO shuffle the cards 
+    }
+}
---
+++ Concentration/ViewController 3.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Evgen on 14.02.2021.
+//  Copyright © 2021 Nikko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+   private lazy var game = ConcentrationGame(numberOfPairsOfCards: (buttonCollection.count + 1) / 2)
+    
+   private(set) var touches = 0 {
+        didSet{
+            touchLabel.text = "Touches: \(touches)"
+        }
+    }
+    
+
+   private var emojiCollection = ["🐗","🐺","🐻","🐨","🐯","🦁","🐮","🐷","🐸","🐵","🐔","🐥","🦆"]
+    var emojiDictionary = [Int:String]()
+    
+   private func emojiIdentifier(for card: Card) -> String {
+        if emojiDictionary[card.identifier] == nil {
+            emojiDictionary[card.identifier] = emojiCollection.remove(at: emojiCollection.count.arc4randomExtension)
+        }
+        return emojiDictionary[card.identifier] ?? "?"
+    }
+    
+   private func updateViewFromModel() {
+        for index in buttonCollection.indices {
+            let button = buttonCollection[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emojiIdentifier(for: card), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+            }
+        }
+    }
+    
+    
+    @IBOutlet private var buttonCollection: [UIButton]!
+    @IBOutlet private weak var touchLabel: UILabel!
+    @IBAction private func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            game.chooseCard(at: buttonIndex)
+            updateViewFromModel()
+        }
+    }
+    
+}
+
+extension Int {
+    var arc4randomExtension: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
