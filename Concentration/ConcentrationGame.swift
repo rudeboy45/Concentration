321f9437c9a631bb32b82a11d70dcae95a9f8318
rudeboy45 <45rudeboy@gmail.com>
2021-07-05 18:28:23 +0300

    filter add

--- Concentration/ConcentrationGame.swift
+++ Concentration/ConcentrationGame.swift
@@ -14,17 +14,20 @@ class ConcentrationGame {
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+//            return FaceUpCardIndices.count == 1 ? FaceUpCardIndices.first : nil
+            
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -55,6 +58,13 @@ class ConcentrationGame {
             cards += [card,card]
             
         }
-        // TODO shuffle the cards 
+        // TODO shuffle the cards
+    }
+    
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
